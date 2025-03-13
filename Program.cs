aef6c30c59b7b971ecb17807a0a73acf4d3acdaf
Sany <sany.baust@gmail.com>
2025-03-14 00:24:05 +0600

    updated

--- Program.cs
+++ Program.cs
@@ -2,25 +2,30 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Map OpenAPI endpoint
     app.MapOpenApi();
+    
+    // Don't require HTTPS in development
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
-
-
+else
+{
+    // In production, enforce HTTPS
+    app.UseHttpsRedirection();
+}
 
 app.MapGet("/", () =>
 {
     return "Job Portal";
 })
-.WithName("JobPortal");
+.WithName("JobpPortal");
 
 app.Run();
 
